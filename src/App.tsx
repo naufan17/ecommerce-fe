b1e5c2c8234d4c8759410dfb065d2b7bd5bbc295
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-12-11 09:19:15 +0700

    chore: setup project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-500">Hellooo Ecommerce</h1>
+    </div>
+  )
+}
+
+export default App
